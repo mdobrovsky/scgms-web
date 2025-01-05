f55d69a20ccdcb1e828e4e0475adb1b40a7d6deb
mdobrovsky <mdobrovsky88@gmail.com>
2025-01-05 20:30:57 +0100

    remove useless

--- test.py
+++
@@ -1,7 +0,0 @@
-import scgms_wrapper  # Importuj váš C++ modul
-
-
-# Vypiš výsledek
-print(f"Is scgms lib loaded: {scgms_wrapper.load_scgms_lib()}")
-print(f"Available filters: {scgms_wrapper.list_available_filters()}")
-
